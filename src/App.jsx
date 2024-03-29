59ff63445677a06342d9015ec91ca11a5e6dbffa
Vasanthan <srvasanthan33@outlook.com>
2024-03-29 13:55:43 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+import "./App.css"
+
+const App = () => {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default App
